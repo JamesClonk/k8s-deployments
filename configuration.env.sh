710e8035699cedec0559aa227b0e0fc16631c934
JamesClonk <jamesclonk@jamesclonk.ch>
2020-11-29 00:45:14 +0100

    add pgweb

--- configuration.env.sh
+++ configuration.env.sh
@@ -20,3 +20,21 @@ export INGRESS_BASIC_AUTH_PASSWORD="${INGRESS_BASIC_AUTH_PASSWORD:=not-secret}" 
 ########################################################################################################################
 export REPO_MIRRORER_GITLAB_TOKEN="${REPO_MIRRORER_GITLAB_TOKEN:=my-gitlab-token}" # your gitlab token
 ########################################################################################################################
+
+
+########################################################################################################################
+####### databases ######################################################################################################
+########################################################################################################################
+export JCIO_MOVIEDB_HOST="${JCIO_MOVIEDB_HOST:=host}"
+export JCIO_MOVIEDB_NAME="${JCIO_MOVIEDB_NAME:=dbname}"
+export JCIO_MOVIEDB_USERNAME="${JCIO_MOVIEDB_USERNAME:=username}"
+export JCIO_MOVIEDB_PASSWORD="${JCIO_MOVIEDB_PASSWORD:=password}"
+export HOME_INFO_DB_HOST="${HOME_INFO_DB_HOST:=host}"
+export HOME_INFO_DB_NAME="${HOME_INFO_DB_NAME:=dbname}"
+export HOME_INFO_DB_USERNAME="${HOME_INFO_DB_USERNAME:=username}"
+export HOME_INFO_DB_PASSWORD="${HOME_INFO_DB_PASSWORD:=password}"
+export IR_VISUALIZER_DB_HOST="${IR_VISUALIZER_DB_HOST:=host}"
+export IR_VISUALIZER_DB_NAME="${IR_VISUALIZER_DB_NAME:=dbname}"
+export IR_VISUALIZER_DB_USERNAME="${IR_VISUALIZER_DB_USERNAME:=username}"
+export IR_VISUALIZER_DB_PASSWORD="${IR_VISUALIZER_DB_PASSWORD:=password}"
+########################################################################################################################
