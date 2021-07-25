32f202e77dac55e83c73393069f1da7613a1d56c
JamesClonk <jamesclonk@jamesclonk.ch>
2021-07-25 14:01:15 +0200

    switch to SOPS, remove local ingress domain

--- kuard/deploy.sh
+++ kuard/deploy.sh
@@ -5,9 +5,8 @@ source ../setup.sh
 
 # deploy
 echo "deploying [kuard] ..."
-cat values.yml |
-	envsubst -no-unset -no-empty |
-	ytt --ignore-unknown-comments -f templates -f - |
+sops -d ../secrets.sops |
+	ytt --ignore-unknown-comments -f templates -f values.yml -f ../configuration.yml -f - |
 	kbld -f - -f image.lock.yml |
 	kapp deploy -a kuard -c -y -f -
 kapp app-change garbage-collect -a kuard --max 5 -y
