68f6e4840f8cbb0f62e8e00fd97a5316d8919638
JamesClonk <jamesclonk@jamesclonk.ch>
2021-07-25 19:08:16 +0200

    verbose cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -4,11 +4,15 @@ set -u
 source $(dirname ${BASH_SOURCE[0]})/env.sh # source env configuration files
 
 # $HOME/.ssh
+echo "removing writing [$HOME/.ssh/id_rsa] ..."
 rm -f "$HOME/.ssh/id_rsa" || true
 
 # aws config
+echo "removing writing [$HOME/.aws/config] ..."
 rm -f "$HOME/.aws/config" || true
+echo "removing writing [$HOME/.aws/credentials] ..."
 rm -f "$HOME/.aws/credentials" || true
 
 # kubectl config
+echo "removing writing [${KUBECONFIG}] ..."
 rm -f "${KUBECONFIG}" || true
