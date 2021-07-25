d469a3dbedfe1c54536fa3499f917743d61da30b
JamesClonk <jamesclonk@jamesclonk.ch>
2021-07-25 19:31:49 +0200

    fix typo

--- cleanup.sh
+++ cleanup.sh
@@ -4,15 +4,15 @@ set -u
 source $(dirname ${BASH_SOURCE[0]})/env.sh # source env configuration files
 
 # $HOME/.ssh
-echo "removing writing [$HOME/.ssh/id_rsa] ..."
+echo "removing [$HOME/.ssh/id_rsa] ..."
 rm -f "$HOME/.ssh/id_rsa" || true
 
 # aws config
-echo "removing writing [$HOME/.aws/config] ..."
+echo "removing [$HOME/.aws/config] ..."
 rm -f "$HOME/.aws/config" || true
-echo "removing writing [$HOME/.aws/credentials] ..."
+echo "removing [$HOME/.aws/credentials] ..."
 rm -f "$HOME/.aws/credentials" || true
 
 # kubectl config
-echo "removing writing [${KUBECONFIG}] ..."
+echo "removing [${KUBECONFIG}] ..."
 rm -f "${KUBECONFIG}" || true
