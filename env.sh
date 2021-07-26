6c366bf509d6b9954050b0dae766eed3a5a10530
JamesClonk <jamesclonk@jamesclonk.ch>
2021-07-26 13:18:47 +0200

    bump [ci skip]

--- env.sh
+++ env.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 source $(dirname ${BASH_SOURCE[0]})/.env* 1>/dev/null 2>&1 || true # source any hidden env config files if available
+export CONFIGURATION_FILE="$(dirname ${BASH_SOURCE[0]})/configuration.yml"
+export SECRETS_FILE="$(dirname ${BASH_SOURCE[0]})/secrets.sops"
+
 
 ########################################################################################################################
 ####### kubernetes #####################################################################################################
