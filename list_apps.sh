dbf476295cf4962e5da4ffaba8480d65bfc00b2e
JamesClonk <jamesclonk@jamesclonk.ch>
2026-03-28 13:56:33 +0100

    debug

--- list_apps.sh
+++ list_apps.sh
@@ -6,4 +6,7 @@ source $(dirname ${BASH_SOURCE[0]})/setup.sh
 
 # list apps
 echo "displaying all apps ..."
+echo ${KUBECONFIG} || true
+kubectl cluster-info || true
+kubectl get ns || true
 kapp list
